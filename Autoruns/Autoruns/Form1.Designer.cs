3393e7f3a25da78865c59b75e2b657e66690cdd3
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-05-16 08:19:18 +0800

    first commit

---
+++ Autoruns/Autoruns/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Autoruns
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.entryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hideEmptyLocationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hideMicrosoftEntrysToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hideWindowsEntriesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 749);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1522, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(131, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.entryToolStripMenuItem,
+            this.optionsToolStripMenuItem,
+            this.userToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1522, 25);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 21);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // entryToolStripMenuItem
+            // 
+            this.entryToolStripMenuItem.Name = "entryToolStripMenuItem";
+            this.entryToolStripMenuItem.Size = new System.Drawing.Size(49, 21);
+            this.entryToolStripMenuItem.Text = "Entry";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hideEmptyLocationToolStripMenuItem,
+            this.hideMicrosoftEntrysToolStripMenuItem,
+            this.hideWindowsEntriesToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(66, 21);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // userToolStripMenuItem
+            // 
+            this.userToolStripMenuItem.Name = "userToolStripMenuItem";
+            this.userToolStripMenuItem.Size = new System.Drawing.Size(47, 21);
+            this.userToolStripMenuItem.Text = "User";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem1,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(47, 21);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // hideEmptyLocationToolStripMenuItem
+            // 
+            this.hideEmptyLocationToolStripMenuItem.Name = "hideEmptyLocationToolStripMenuItem";
+            this.hideEmptyLocationToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.hideEmptyLocationToolStripMenuItem.Text = "Hide Empty Locations";
+            // 
+            // hideMicrosoftEntrysToolStripMenuItem
+            // 
+            this.hideMicrosoftEntrysToolStripMenuItem.Name = "hideMicrosoftEntrysToolStripMenuItem";
+            this.hideMicrosoftEntrysToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.hideMicrosoftEntrysToolStripMenuItem.Text = "Hide Microsoft Entries";
+            this.hideMicrosoftEntrysToolStripMenuItem.Click += new System.EventHandler(this.hideMicrosoftEntrysToolStripMenuItem_Click);
+            // 
+            // hideWindowsEntriesToolStripMenuItem
+            // 
+            this.hideWindowsEntriesToolStripMenuItem.Name = "hideWindowsEntriesToolStripMenuItem";
+            this.hideWindowsEntriesToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.hideWindowsEntriesToolStripMenuItem.Text = "Hide Windows Entries";
+            // 
+            // helpToolStripMenuItem1
+            // 
+            this.helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
+            this.helpToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.helpToolStripMenuItem1.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 649);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1522, 100);
+            this.panel1.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 25);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1522, 624);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1522, 771);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Autoruns";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem entryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hideEmptyLocationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hideMicrosoftEntrysToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem userToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hideWindowsEntriesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Autoruns/Autoruns/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Autoruns
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitListView();
+        }
+
+        private void InitListView()
+        {
+            listView1.View = View.Details;
+            listView1.AllowColumnReorder = false;
+            listView1.FullRowSelect = true;
+            listView1.HeaderStyle = ColumnHeaderStyle.Nonclickable;
+            
+            // redraw
+            listView1.OwnerDraw = true;
+            listView1.DrawItem += new DrawListViewItemEventHandler(listView1_DrawItem);
+            listView1.DrawSubItem += new DrawListViewSubItemEventHandler(listView1_DrawSubItem);
+            listView1.DrawColumnHeader += new DrawListViewColumnHeaderEventHandler(listView1_DrawColumnHeader);
+
+            // add five columns
+            listView1.Columns.Add("Autorun Entry", 160, HorizontalAlignment.Left);
+            listView1.Columns.Add("Description", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Publisher", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Image Path", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Timestamp", 160, HorizontalAlignment.Left);
+
+            ListViewItem item1 = new ListViewItem("11111111111111111111111111111111111111111111111111111111111111111111111", 0);
+            item1.SubItems.Add("");
+            item1.SubItems.Add("");
+            item1.SubItems.Add("John Smith");
+            item1.SubItems.Add("John Smith");
+            listView1.Items.Add(item1);
+            // Create an image list and add an image.
+            ImageList list = new ImageList();
+            list.Images.Add(new Bitmap(typeof(Button), "Button.bmp"));
+            // SmallImageList must be set when using IndentCount.
+            listView1.SmallImageList = list;
+        }
+
+        private void hideMicrosoftEntrysToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Draws the backgrounds for entire ListView items.
+        private void listView1_DrawItem(object sender, DrawListViewItemEventArgs e)
+        {
+            if (e.Item.IndentCount != 0)
+                e.DrawDefault = true;
+        }
+
+        // Draws subitem text and applies content-based formatting.
+        private void listView1_DrawSubItem(object sender, DrawListViewSubItemEventArgs e)
+        {
+            // Check if e.Item is a Reg Key.
+            if (e.Item.IndentCount == 0)
+            {
+                ListView listView = (ListView)sender;
+                if (e.ColumnIndex == 0)
+                {
+                    Rectangle rowBoundsL = e.Item.SubItems[0].Bounds;
+                    Rectangle rowBoundsR = e.Item.SubItems[4].Bounds;
+                    Rectangle bounds = new Rectangle(rowBoundsL.Left,
+                        rowBoundsL.Top,
+                        rowBoundsR.Left - rowBoundsL.Left,
+                        rowBoundsR.Height);
+                    TextRenderer.DrawText(e.Graphics,
+                        e.Item.SubItems[0].Text,
+                        listView.Font,
+                        bounds,
+                        Color.Black,
+                        TextFormatFlags.Left | TextFormatFlags.SingleLine | TextFormatFlags.GlyphOverhangPadding | TextFormatFlags.VerticalCenter | TextFormatFlags.WordEllipsis);
+                }
+                else if (e.ColumnIndex == 4)
+                {
+                    TextRenderer.DrawText(e.Graphics,
+                        e.Item.SubItems[4].Text,
+                        listView.Font,
+                        e.Item.SubItems[4].Bounds,
+                        Color.Black,
+                        TextFormatFlags.Left);
+                }
+
+            }
+            else
+                e.DrawDefault = true;
+        }
+
+        // Draws column headers.
+        private void listView1_DrawColumnHeader(object sender, DrawListViewColumnHeaderEventArgs e)
+        {
+            e.DrawDefault = true;
+        }
+
+    }
+}
